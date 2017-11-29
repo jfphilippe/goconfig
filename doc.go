a200c916efb32697c144af15cacba738c589d071
Jean-Francois PHILIPPE <428086+jfphilippe@users.noreply.github.com>
2017-11-29 14:25:18 +0100

    Fix mix-indent warning

--- doc.go
+++ doc.go
@@ -5,30 +5,30 @@ Package goconfig implements a config object initialised from files and env varia
 
 Usage
 
-     builder := NewBuilder("CTX_", nil)
-	 // Main config values
-	 _, err := builder.LoadJsonFile("/etc/myapp/config.json")
+	builder := NewBuilder("CTX_", nil)
+	// Main config values
+	_, err := builder.LoadJsonFile("/etc/myapp/config.json")
 
-	 // complete config with values from another file
-	 // existing values will NOT be updated.
-	 _,err = builder.LoadTxtFile("/etc/default/mayapp.txt")
+	// complete config with values from another file
+	// existing values will NOT be updated.
+	_,err = builder.LoadTxtFile("/etc/default/mayapp.txt")
 
-	 // OR
-	 // _,err := builder.LoadFiles("/etc/myapp/config.json","/etc/default/mayapp.txt")
-	 // Use it
-	 config := builder.Config()
+	// OR
+	// _,err := builder.LoadFiles("/etc/myapp/config.json","/etc/default/mayapp.txt")
+	// Use it
+	config := builder.Config()
 
-	 name := config.GetString("name", "default name")
-	 num_proc := config.GetInt("proc.limit", 5)
+	name := config.GetString("name", "default name")
+	num_proc := config.GetInt("proc.limit", 5)
 
-	 dbconfig := config.GetConfig("database")
+	dbconfig := config.GetConfig("database")
 
-	 db_url := dbconfig.GetString("url", "")
-	 db_port := dbconfig.GetInt("port", 1234)
+	db_url := dbconfig.GetString("url", "")
+	db_port := dbconfig.GetInt("port", 1234)
 
-	 // Or
-	 // db_url := config.GetString("database.url","")
-	 ...
+	// Or
+	// db_url := config.GetString("database.url","")
+	...
 
 Config store values as nested map[string]interface{}.
 
